809cc3675fc82dea949ae5ca5900e2be44318a5b
PAULADEJUANSEGURA <pauladejuansegura@outlook.es>
2026-04-17 10:40:55 +0200

    Subimos OperadoresMatematicos y OperadoresComparacion

---
+++ 1_basicos/operadorescomparacion.py
@@ -0,0 +1,29 @@
+# Operadores de Comparación
+# Usamos operadores y nos devuelven valores BOOLEANOS
+numA = 18
+numB = 5
+
+# Mayor que
+resultado = numA > numB
+print("Mayor que: ¿Es 18 > que 5?", resultado)
+
+resultado2 = numA >= numB
+print("Mayor o igual que: ¿Es 18 mayor o igual que 5?", resultado2)
+
+# Menor que
+resultado3 = numA < numB
+print("Menor que: ¿Es 18 > que 5?", resultado3)
+
+resultado4 = numA <= numB
+print("Menor o igual que: ¿Es 18 menor o igual que 5?", resultado4)
+
+
+#Igualdad
+resultado = numA == numB
+print("¿Es igual numA(18) a numB(5)?", resultado)
+
+#Distinto
+resultado = numA != numB
+print("¿Es 18 distinto de 5?", resultado)
+
+
---
+++ 1_basicos/operadoresmatematicos.py
@@ -0,0 +1,48 @@
+# operador de asignacion es el igual (=)
+numero = 5
+
+ # suma
+print(5 + 8)
+
+#con variables la suma
+numA = 31
+numB = 12
+print(numA + numB)
+
+resultado = numA + numB
+print(resultado + 10)
+
+#resta
+numC = 15
+print(resultado - numC)
+print(numA - numB)
+
+#multiplicación
+numD = 2
+print(resultado * numD)
+
+#division
+print(type(numA / numB))
+
+#potencia con el doble asterisco
+print(2**3)
+
+#division entera
+print("Division entera:", 18 // 5)
+
+#resto de la division operador MODULO 
+print("Operador Módulo:", numA % numD)
+
+# incrementro / decremento / asignacion
+numero = 2
+numero = numero + 2
+print("Incremento:", numero)
+numero += 2 #Esto es lo mismo que numero = numero + 2
+print("Incremento por segunda vez", numero)
+
+numero -= 3 #Esto es igual numero = numero - 3
+numero += 1 #Esto es igual numero = numero + 1
+print("Despues de operaciones", numero)
+
+
+
