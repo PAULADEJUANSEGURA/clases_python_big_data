6c36c78b3bf2927ae86c13fbb7f73559356184d5
PAULADEJUANSEGURA <pauladejuansegura@outlook.es>
2026-04-21 10:49:07 +0200

    Tablas multiplicar con for

--- 2_flujo/bucles/ejercicio-for.py
+++ 2_flujo/bucles/ejercicio-for.py
@@ -6,3 +6,10 @@ numero_tabla = int(input("Dime el numero del que quieras la tabla: "))
 for i in range(11):
     resultado = numero_tabla * i
     print(f"{numero_tabla} x {i} = {resultado}")
+    
+
+print("----- version 2 --------- ")    
+# version 2 del Ejercicio FOR
+numero = int(input("Dime un numero: "))
+for i in range(1, 10):
+    print(f"{numero} x {i} = {numero * i}")
