adfd4a1b4c22ac23263f388fca547204c88f81ce
PAULADEJUANSEGURA <pauladejuansegura@outlook.es>
2026-04-23 13:30:18 +0200

    Funciones usuales con Strings

--- 2_flujo/funciones/ej5_funcion_devuelve_solo_iniciales.py
+++ 2_flujo/funciones/ej5_funcion_devuelve_solo_iniciales.py
@@ -1,12 +1,30 @@
 # # Realizar una funcion que me introduzca un texto y me cuente sus vocales.
+print("------------- Obtener iniciales version propia--------------")
 nombre_y_apellidos = input("Dime tu nombre y apellidos: ")
 
 def solo_iniciales(nombre_y_apellidos):
     cantidad = len(nombre_y_apellidos)
+    resultado = ""
     for i in range (cantidad):
         if i == 0:
-            print(f"{nombre_y_apellidos[i].upper()}.")
+            resultado = resultado + nombre_y_apellidos[i].upper()+  ". "
         if nombre_y_apellidos[i] == " ":
-            print(f"{nombre_y_apellidos[i + 1].upper()}.")
-          
+            resultado = resultado + nombre_y_apellidos[i + 1].upper()+ ". "
+    print(f"Las iniciales son {resultado}")
+    
 solo_iniciales(nombre_y_apellidos)
+
+
+print("------------------Obtener iniciales version 2 -------------")
+nombre2 = input("Dime un nombre y apellidos: ")
+def obtener_iniciales(nombre2):
+    resultado = ""
+    for i in range (len(nombre2)):
+        if i == 0:
+            resultado = resultado + nombre2[i].upper() + ". "
+        if nombre2[i] == " ":
+            resultado = resultado + nombre2[i+1].upper() + ". "
+    print(resultado)
+
+obtener_iniciales(nombre2)
+# obtener_iniciales("Juan Antonio Perez")
---
+++ 2_flujo/funciones/funciones_usuales/cadenas_texto.py
@@ -0,0 +1,42 @@
+# Funciones para las cadenas de texto:
+
+# Paradigma FUNCIONAL
+# nombre_funcion(texto)
+# len(nombre)
+
+# Paradigma Orientado a Objetos
+# texto.nombre_funcion()
+# nombre.upper()
+
+
+texto = "hOlA"
+print(texto.lower()) # hola
+print(texto.upper()) # HOLA
+# lower() y upper() no cambian el valor del texto original
+print(texto)
+
+titulo = "Hola COMO estas?"
+# Primera letra de toda la frase en Mayuscula
+print(titulo.capitalize()) # Hola como estas
+# Convertir la primera Letra de cada palabra en Mayuscula
+print(titulo.title()) # Hola Como Estas
+
+print(texto.swapcase()) # hOlA -> HoLa
+
+
+# funciones de comprobación
+dni = "2157819M"
+print(dni.zfill(9))
+
+
+frase = "En un lugar de la ManchA..."
+print(frase.lower().count('a'))
+ciudad = "Alcarria"
+frase_modificada = frase.replace("ManchA", ciudad)
+print(frase)
+print(frase_modificada)
+
+# En esta frase: ¿Como están los máquinas?
+frase_con_espacios = "¿Como están los máquinas?"
+frase_sin_espacios = frase_con_espacios.replace(" ", "")
+print(frase_sin_espacios)
