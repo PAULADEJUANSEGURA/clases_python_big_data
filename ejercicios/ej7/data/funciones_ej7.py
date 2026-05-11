f62b56fbe7bbfed43bfea7c117a87460fa383324
PAULADEJUANSEGURA <pauladejuansegura@outlook.es>
2026-05-11 21:15:16 +0200

    funcionalidad de crear archivo, añadir producto a lista de la compra, mostrar lista en color amarillo, borrar lista, y salir del programa | Manejo de ficheros

--- ejercicios/ej7/data/funciones_ej7.py
+++ ejercicios/ej7/data/funciones_ej7.py
@@ -1,25 +1,43 @@
 # Funciones del Ejercicio 7 
-
+lista_de_la_compra = []
 
 def crear_fichero(nombre_fichero, ruta, extension):         
     path = f"{ruta}{nombre_fichero}{extension}"
-    print(path)
-    mi_fichero = open(path, 'w', encoding='UTF-8')
+    # print(path)
+    mi_fichero = open(path, 'x', encoding='UTF-8')
     # Cerramos fichero
     mi_fichero.close()
 
 
 
-def actualizar_datos(nombre_fichero, ruta, extension, datos):
+def actualizar_datos(nombre_fichero, ruta, extension):
     path = f"{ruta}{nombre_fichero}{extension}"
-    print(path)
+    # print(path)
     mi_fichero = open(path, 'a', encoding='UTF-8')
     nombre = input("Dime el nombre del producto: ")
     precio = input("Dime el precio del producto: ")
-    return mi_fichero.write(f"{nombre}: {precio}\n")
+    mi_fichero.write(f"{nombre}: {precio}\n")
+    # Cerramos fichero
+    mi_fichero.close()
 
 
-"""def leer_fichero():
-    mi_fichero = open('./lista_de_la_compra.txt', 'r', encoding='UTF-8')
-    for linea in mi_fichero.readlines():
-"""
+def leer_fichero(nombre_fichero, ruta, extension):
+    path = f"{ruta}{nombre_fichero}{extension}"
+    # print(path)
+    mi_fichero = open(path, 'r', encoding='UTF-8')
+    for producto in mi_fichero.readlines():
+        print(f"Producto: \033[33m{producto}\033[0m")
+        lista_de_la_compra.append(producto)
+    # else:
+        # print(lista_de_la_compra)      
+    # Cerramos fichero  
+    mi_fichero.close()
+
+
+def sobrescribir_fichero(nombre_fichero, ruta, extension):
+    path = f"{ruta}{nombre_fichero}{extension}"
+    # print(path)
+    mi_fichero = open(path, 'w', encoding='UTF-8')
+    mi_fichero.write("")    
+    # Cerramos fichero
+    mi_fichero.close()
