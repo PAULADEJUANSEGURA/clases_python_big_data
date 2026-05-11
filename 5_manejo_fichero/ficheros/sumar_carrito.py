18b53ee7be3b42102992551b6887a1a06740ccf7
XTALENTO102\UsuarioM <pauladejuansegura@outlook.es>
2026-05-11 11:06:44 +0200

    Leer ficheros.txt Sumar carrito, carrito y productos

--- 5_manejo_fichero/ficheros/sumar_carrito.py
+++ 5_manejo_fichero/ficheros/sumar_carrito.py
@@ -1,20 +1,24 @@
 # Funcion que reciba el nombre del fichero, lo abrirá, lo leerá y que devuelva la suma total de todos los productos del carrito
 
-mi_carrito = open('./carrito.txt', 'r', encoding='UTF-8')
+def sumar_carrito(nombre_fichero):
+    try:
+        mi_carrito = open(f"{nombre_fichero}.txt", 'r', encoding='UTF-8')
 
-carrito = []
+        carrito = []
 
-for linea in mi_carrito.readlines():
-    linea = linea.replace('\n', "")
-    linea = linea.split(" ")
-    print(linea)
-    carrito.append(int(linea[1]))
-else:   
-    print(carrito)
-    suma = sum(carrito)
-    print(suma)
+        for linea in mi_carrito.readlines():
+            linea = linea.replace('\n', "")
+            linea = linea.split(" ")
+            print(linea)
+            carrito.append(float(linea[1]))
+        else:   
+            print(carrito)
+            suma = sum(carrito)
+            print(suma)
+        # MUY IMPORTANTE: Cerrar el fichero
+        mi_carrito.close()
+    except FileNotFoundError:
+        print("El fichero no existe")
 
-
-# MUY IMPORTANTE: Cerrar el fichero
-mi_carrito.close()
-
+sumar_carrito('carrito')
+sumar_carrito('productos')
