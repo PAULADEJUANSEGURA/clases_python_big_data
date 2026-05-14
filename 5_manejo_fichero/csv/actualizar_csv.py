eb053acf5a0128d3ee9c3d87fface7701899d2a6
XTALENTO102\UsuarioM <pauladejuansegura@outlook.es>
2026-05-14 10:14:00 +0200

    actualizar csv.py

--- 5_manejo_fichero/csv/actualizar_csv.py
+++ 5_manejo_fichero/csv/actualizar_csv.py
@@ -1,24 +1,30 @@
 import csv
 
-def actualizar_datos(carpeta, nombre):
-    empleado_nuevo = pedir_datos()
-    fichero = open(f'./{carpeta}/{nombre}', 'a', encoding='UTF-8', newline="")
-    mi_csv = csv.DictWriter(fichero)
-    mi_csv.writerow(empleado_nuevo)
-
-id = 6
+def extraer_ultimo_id(carpeta, nombre):
+    fichero = open(f'./{carpeta}/{nombre}', 'r', encoding='UTF-8')
+    datos = list( csv.DictReader(fichero) )
+    return int(datos[-1]['id']) + 1
+    
 def pedir_datos():
-    global id 
     nombre = input("Nombre: ")
     apellidos = input("Apellidos: ")
     correo = input("Correo: ")
-    dpto = input("Departamento: ")
-    id += 1
-    return {
-            "id": id,
-            "nombre": nombre,
-            "apellidos": apellidos,
-            "correo": correo,
-            "departamento": dpto
-                }
+    dpto = input('Departamento: ')
+    return [nombre,apellidos ,correo, dpto]
+
+
+def actualizar_datos(carpeta, nombre):  
+    try:
+        empleado_nuevo = pedir_datos()
+        # añadir el siguiente id al nuevo empleado
+        empleado_nuevo.insert(0, extraer_ultimo_id(carpeta, nombre)) 
+        fichero = open(f"./{carpeta}/{nombre}", 'a', encoding='UTF-8', newline="")
+        mi_csv = csv.writer(fichero)
+        mi_csv.writerow(empleado_nuevo)
+        print('El archivo ha sido actualizado')
+        fichero.close()
+    except:
+        print('El archivo no ha podido actualizarse') 
+    
+actualizar_datos('data', 'empleados.csv')
 
