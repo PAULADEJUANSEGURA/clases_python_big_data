68b53b09424e90ddbb0e29b2b1c5a1b878b87ddb
XTALENTO102\UsuarioM <pauladejuansegura@outlook.es>
2026-05-22 10:31:48 +0200

    cargamos artistas.csv, cargar patrocinadores.xml, cargar ventas entradas.json

--- practica_final_opcionA/main.py
+++ practica_final_opcionA/main.py
@@ -3,11 +3,13 @@ import lib.carga as cargar
 
 # Definimos funcion principal main
 def main():
+    # Leer artistas
     # cargar artistas.csv
-    # cargar.cargar_csv('datos', 'artistas.csv')
+    resultado_artistas = cargar.cargar_csv('datos', 'artistas.csv')
+    print(resultado_artistas)
     # cargar patrocinadores.xml
-    # resultado_patrocinadores = cargar.cargar_patrocinadores('datos', 'patrocinadores.xml')
-    # print(resultado_patrocinadores)
+    resultado_patrocinadores = cargar.cargar_patrocinadores('datos', 'patrocinadores.xml')
+    print(resultado_patrocinadores)
     # Leer venta_entradas.json
     ventas_entradas = cargar.leer_json('datos', "ventas_entradas.json")
     print(ventas_entradas)
