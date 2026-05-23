fa1779a02022eccba686f70f2929d701b974f80a
PAULADEJUANSEGURA <pauladejuansegura@outlook.es>
2026-05-23 18:09:31 +0200

    cambios y funcion mostrar_resumen para visualizar mejor los 5 primeros registros de cada archivo

--- practica_final_opcionA/lib/carga.py
+++ practica_final_opcionA/lib/carga.py
@@ -17,26 +17,26 @@ from openpyxl import load_workbook, Workbook
 
 # Creo función para cargar csv (artistas)
 def cargar_csv(carpeta, nombre_fichero):
-    lista_artistas = []
     # Cargo archivo csv
     fichero_csv = open(f"./{carpeta}/{nombre_fichero}", 'r', encoding='UTF-8')
     # Creo elemento lector que me va a permitir leer el csv
-    lector = csv.reader(fichero_csv)
+    lector = csv.DictReader(fichero_csv)
     # si el archivo tiene cabeceras me las salto
     next(lector)
+    lista_artistas = []
     for fila in lector:
-        lista_artistas.append(fila)
+        lista_artistas.append(dict(fila))
     return lista_artistas
 
 
 # Creo función para leer patrocinadores.xml
-def cargar_patrocinadores(carpeta, nombre_fichero):
+def cargar_xml(carpeta, nombre_fichero):
     # Para leer un fichero XML, lo primero que tenemos que hacer es cargar el fichero en memoria. Para ello, utilizamos la función parse() de la librería xml.etree.ElementTree. Esta función recibe como parámetro el nombre del fichero XML que queremos cargar.
     mi_fichero_xml = et.parse(f"./{carpeta}/{nombre_fichero}")
      # Una vez que hemos cargado el fichero XML en memoria, podemos acceder a su contenido. Para ello, utilizamos la función getroot() de la clase ElementTree. Esta función nos devuelve el elemento raíz del árbol XML.
     nodo_raiz = mi_fichero_xml.getroot()
     # Ahora que tenemos el nodo raíz, podemos acceder a sus hijos. Para ello, utilizamos la función findall() del nodo raíz. Esta función recibe como parámetro el nombre del elemento que queremos buscar. En nuestro caso, queremos buscar todos los elementos
-    lista_patrocinadores = []
+    lista = []
     for patrocinador in nodo_raiz.findall('patrocinador'):
         patrocinador_dict ={
         "nombre_empresa": patrocinador.find("nombre_empresa").text,
@@ -47,19 +47,19 @@ def cargar_patrocinadores(carpeta, nombre_fichero):
         "fecha_inicio": patrocinador.find("fecha_inicio").text,
         "fecha_fin": patrocinador.find("fecha_fin").text,
         }
-        lista_patrocinadores.append(patrocinador_dict)
+        lista.append(patrocinador_dict)
     
-    return lista_patrocinadores
+    return lista
 
 
 # Definimos función de carga de archivos json (venta entradas)
-def leer_json(carpeta, nombre_fichero):
+def cargar_json(carpeta, nombre_fichero):
     fichero = open(f"./{carpeta}/{nombre_fichero}", 'r', encoding='UTF-8')
     ventas_entradas_datos = json.load(fichero)
     return ventas_entradas_datos
 
 # Definimos función para obtener los Excel (Escenarios_horarios.xlsx)
-def leer_excel_escenarios(carpeta, nombre_fichero):
+def cargar_excel(carpeta, nombre_fichero):
     #cargar el fichero de excel en nuestro archivo
     libro_excel = load_workbook(f'./{carpeta}/{nombre_fichero}')
     hoja = libro_excel.active
@@ -68,8 +68,23 @@ def leer_excel_escenarios(carpeta, nombre_fichero):
     filas = hoja.iter_rows(values_only = True)
     cabeceras = next(filas)
     # Iterar sobre las filas de la hoja
-    for fila in hoja.iter_rows(min_row=2, values_only=True):
+    for fila in filas:
         escenarios_dict = dict(zip(cabeceras, fila))
         escenarios_horarios.append(escenarios_dict)
     
     return escenarios_horarios
+
+def mostrar_resumen(nombre_fichero, datos):
+
+    print(f"\n=== {nombre_fichero} ===")
+
+    print("Total registros:", len(datos))
+
+    print("Columnas:", list(datos[0].keys()))
+
+    print("\nPrimeros 5 registros:")
+
+    for registro in datos[:5]:
+        print(registro)
+
+
--- practica_final_opcionA/main.py
+++ practica_final_opcionA/main.py
@@ -2,25 +2,35 @@
 import lib.carga as cargar
 import lib.limpieza as limpiar
 
+
 # Definimos funcion principal main
 def main():
-    # Leer artistas cargar artistas.csv
+    """# Leer artistas cargar artistas.csv
     lista_artistas = cargar.cargar_csv('datos', 'artistas.csv')
     # lista_empleados = cargar.cargar_csv('datos', 'empleados.csv' )
     print(lista_artistas)
-    print("-" * 60)
     resultado_limpiar_artistas = limpiar.limpiar_texto(lista_artistas, mayusculas=False)
     # resultado_limpiar_empleados = limpiar.limpiar_texto(lista_empleados, mayusculas=False)
     # print(resultado_limpiar_empleados)
-""" # cargar patrocinadores.xml
-    resultado_patrocinadores = cargar.cargar_patrocinadores('datos', 'patrocinadores.xml')
+    # cargar patrocinadores.xml
+    resultado_patrocinadores = cargar.cargar_xml('datos', 'patrocinadores.xml')
     print(resultado_patrocinadores)
     # Leer venta_entradas.json
-    ventas_entradas = cargar.leer_json('datos', "ventas_entradas.json")
+    ventas_entradas = cargar.cargar_json('datos', "ventas_entradas.json")
     print(ventas_entradas)
-    resultado_escenarios_horarios = cargar.leer_excel_escenarios('datos', "escenarios_horarios.xlsx")
+    resultado_escenarios_horarios = cargar.cargar_excel('datos', "escenarios_horarios.xlsx")
     print(resultado_escenarios_horarios)"""
 
+artistas = cargar.cargar_csv("datos", "artistas.csv")
+ventas_entradas = cargar.cargar_json("datos", "ventas_entradas.json")
+patrocinadores = cargar.cargar_xml("datos", "patrocinadores.xml")
+escenarios = cargar.cargar_excel("datos", "escenarios_horarios.xlsx")
+
+cargar.mostrar_resumen("artistas.csv", artistas)
+cargar.mostrar_resumen("ventas_entradas.json", ventas_entradas)
+cargar.mostrar_resumen("patrocinadores.xml", patrocinadores)
+cargar.mostrar_resumen("escenarios_horarios.xlsx", escenarios)
 
 
 main()
+
